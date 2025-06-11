3527d284d4a655a4f69473a3f6910e8888e174a8
locle27 <loc22100302@gmail.com>
2025-06-12 02:23:25 +0700

    nEW FIX 2

---
+++ fix_git.ps1
@@ -0,0 +1,4 @@
+Set-Location "C:\Users\T14\Desktop\hotel_flask_app"
+git add templates/base.html
+git commit -m "HOTFIX: Add missing base.html to fix navigation BuildError"
+git push origin clean-main
